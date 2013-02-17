348dd8bd109b7748c6ce300a3cc3cabf062d6f4f
Jason Zerbe <jzerbe@vraidsys.com>
2013-02-16 21:30:56 -0700

    remove overkill Ant process in favor of shell script as originally intended

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ django-nonrel based project for using Google App Engine as a RESTful blobstore
 
 storagebin is based on the [django-testapp](https://github.com/django-nonrel/django-testapp)
 template for creating a Google App Engine app with [django-nonrel](https://github.com/django-nonrel).
-Run `ant setup` to initialize a local development environment.
+Run `setup.sh APP_ID VERSION_ID` to initialize a local development environment.
 
 Development Dependencies
 ------------
-- [Apache Ant](http://ant.apache.org/bindownload.cgi)
 - a [POSIX](http://en.wikipedia.org/wiki/POSIX#POSIX-oriented_operating_systems)
 shell with `mkdir`, `unzip`, `cp`, `rm`
 - [git](http://git-scm.com/downloads) and [hg](http://mercurial.selenic.com/wiki/Download) binaries in path
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+    echo "usage: $0 APP_ID VERSION_ID"
+    exit 1
+fi
+
+echo APP_ID will be set to: $1
+echo VERSION_ID will be set to: $2
+
+cat app_template.yaml | sed -e 's/application: REPLACE_ME/application: $1/g' > app.yaml
+cat app.yaml | sed -e 's/version: REPLACE_ME/version: $2/g' > app.yaml
+
+exit 0
+
+mkdir build
+
+pip install --download='./build' --no-install -r requirements.txt
+
+unzip -q build/django-autoload-0.01.zip -d build
+unzip -q build/django-dbindexer-0.3.zip -d build
+unzip -q build/django-nonrel-1.3.1.zip -d build
+unzip -q build/djangoappengine-1.0.zip -d build
+unzip -q build/djangotoolbox-0.9.2.zip -d build
+
+cp -r build/django-autoload/autoload ./autoload
+cp -r build/django-dbindexer/dbindexer ./dbindexer
+cp -r build/django-nonrel/django ./django
+cp -r build/djangoappengine/djangoappengine ./djangoappengine
+cp -r build/djangotoolbox/djangotoolbox ./djangotoolbox
+
+rm -r ./djangoappengine/djangoappengine.egg-info
