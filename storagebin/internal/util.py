963e7bc471d49f0a09c6b04e104fe4c71382346e
Jason Zerbe <jzerbe@vraidsys.com>
2013-10-26 20:20:33 -0600

    this is a question!

--- storagebin/internal/util.py
+++ storagebin/internal/util.py
@@ -1,5 +1,5 @@
 def is_image(content_type):
-    """is the given content_type string for an image
+    """is the given content_type string for an image?
     
     Args:
         content_type: string containing Content-Type HTTP header value
